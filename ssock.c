f845eb82f61d5f3b74f0bb14ea404f1d7a7cbaf0
cylian charbonnier <cylian.charbonnier1@gmail.com>
2023-10-27 22:12:38 +0200

    add ssock.h

--- ssock.c
+++ ssock.c
@@ -6,6 +6,14 @@
 #include <sys/un.h>
 #include <string.h>
 #include <unistd.h>
+
+int message(int *sock, char *mess, char *reply){
+  write(*sock, mess, strlen(mess)+1);
+  read(*sock,reply,1024);
+  printf("Message: %s\n", mess);
+  printf("Received: %s\n", reply);
+  return 0;
+}
 int main(int argc, char *argv[]) {
   if (argc < 2) {
     printf("Usage: %s <path> <message>\n", argv[0]);	
@@ -14,34 +22,29 @@ int main(int argc, char *argv[]) {
     printf("\techo \"hello\" | %s /tmp/mysocket\n", argv[0]);
     printf("\tBoth examples will send \"hello\" to server\n");
     printf("Multiple message can be send with one connection using stdin\n");
-
     return 1;
   }
+
+
   int sock = socket(AF_UNIX, SOCK_STREAM, 0);
-  int resv;
   struct sockaddr_un serverAddress = {0};
-
-  serverAddress.sun_family  = AF_UNIX;
+  serverAddress.sun_family  = AF_UNIX; 
   strcpy(serverAddress.sun_path, argv[1]);
+  
   printf("Connecting to %s\n", argv[1]);
-
+  int resv;
   resv=connect(sock, (struct sockaddr*)&serverAddress, SUN_LEN(&serverAddress));
   if (resv < 0)
     return resv;
   printf("Connected\n");
+ 
   char mess[1024];
   char reply[1024];
   size_t n = 0;
   if (argc == 2) {
     while (fgets(mess, 1024, stdin)!=NULL){
-      printf("%s",mess);
-      write(sock, mess, strlen(mess)+1);
-      read(sock,reply,1024);
-      printf("Message: %s\n", mess);
-      printf("Received: %s\n", reply);
-      sleep(1);
+      message(&sock, mess, reply);
     }
-
   }
   else
   {
@@ -51,11 +54,10 @@ int main(int argc, char *argv[]) {
       strcat(mess," ");
       strcat(mess,argv[i]);
     }
-    write(sock, mess, strlen(mess)+1);
+    message(&sock, mess, reply);
   }
-
-  //strcat(mess,"\0");
-  //reply
   close(sock);
   return 0;
 }
+
+
---
+++ ssock.h
@@ -0,0 +1,5 @@
+#ifndef INIT_ssock_h
+#define INIT_ssock_h
+int message(int *sock, char *mess, char *reply);
+
+#endif
